afecee12c2ce449afa42aa3922726ec354ecabac
Max Veasey <majv@administrate.co>
2021-09-10 13:43:53 +0100

    Select decoded id on focus of the encode input field
    I will be using this to predominantly decode encoded ID strings in the
    format NodeType:1 etc. I want this to focus on the decoded int by
    default so I can copy it. The decode input field remains selecting
    the whole encoded ID as that is what I'd want to copy.

--- popup.js
+++ popup.js
@@ -4,16 +4,23 @@ document.getElementById("encode-input").addEventListener("input", e => {
 });
 
 document.getElementById("decode-input").addEventListener("input", e => {
-    const encodeInputField = document.getElementById("encode-input")
-    encodeInputField.className = null;
+    const encodeInput = document.getElementById("encode-input")
+    encodeInput.className = null;
     try {
-        encodeInputField.value = window.atob(e.target.value);
+        encodeInput.value = window.atob(e.target.value);
     } catch {
-        encodeInputField.className = "error"
-        encodeInputField.value = "Invalid Base64 Encoding";
+        encodeInput.className = "error"
+        encodeInput.value = "Invalid Base64 Encoding";
     }
 });
 
-document.getElementById("encode-input").addEventListener("focus", e => e.target.select())
+document.getElementById("encode-input").addEventListener("focus", e => {
+    const splitIndex = e.target.value.indexOf(":")
+    if (splitIndex !== -1) {
+        e.target.setSelectionRange(splitIndex + 1, e.target.value.length)
+    } else {
+        e.target.select()
+    }
+})
 
 document.getElementById("decode-input").addEventListener("focus", e => e.target.select())
